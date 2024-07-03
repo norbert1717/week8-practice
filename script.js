6835bcc00bf765e6f8de744e54278a9807f444d8
norbert1717 <miklosnorbert17@gmail.com>
2024-07-03 18:27:59 +0200

    creating dom from data

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ console.log(data);
 
 fetchData(); 
 
-console.log("before fetch");
-fetch("https://swapi.dev/api/people/")
+/*fetch("https://swapi.dev/api/people/")
     .then(response => response.json())
     .then(data => console.log(data)); // csak a 2. then callback függvényében érhető el a data
-console.log("after fetch");
+*/
